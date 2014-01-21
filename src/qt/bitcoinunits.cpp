36bb6fb030d47b0d2f82053cee4a1b2d0bb253e6
Nick A <nick@Angel.(none)>
2014-01-21 03:27:13 -0600

    Changed all the names from SmallChange to TexCoin (TEX).

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("TEX");
+    case mBTC: return QString("mTEX");
+    case uBTC: return QString::fromUtf8("μTEX");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("TexCoin");
+    case mBTC: return QString("Milli-TexCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-TexCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
